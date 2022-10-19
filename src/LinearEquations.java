a26876ee8961700f71b65f9dd995f6dcbee75659
BT_3W2_24 <henryw48@nycstudents.net>
2022-10-19 09:13:56 -0400

    Linear Equations

--- src/LinearEquations.java
+++ src/LinearEquations.java
@@ -5,27 +5,49 @@ public class LinearEquations {
     private int y2;
 
     public LinearEquations(int x1, int y1, int x2, int y2){
-       // this.x1 = x1;
+        this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
-
+//distance method
     public double distance(){
 double dis = Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2);
 return Math.pow(dis, 0.5);
     }
+//slope method
     public double slope(){
         return ((double) y2-y1)/(x2-x1);
     }
-    public String equation(){
-        double b = y1-(slope()*x1);
-return "y= " + slope() + "x " + "+ " + b;
-    }
+
+    //y-intercept method
     public double yIntercept(){
         double b = y1-(slope()*x1);
-        return (-b)/slope();
+        return b;
     }
+    //equation method
+    public String equation(){
+        if (slope() == 0){
+            return "y = " + yIntercept();
+        } else if (slope()==1) {
+            return "y = x + " + yIntercept();
+        } else if (slope() == -1) {
+            return "y = -x + " + yIntercept();
+        } else{
+            return "y = " + slope() + "x " + "+ " + yIntercept();
+        }
+    }
+
+
+//input your x value method
+    public String chooseVal(double yourX){
+        double yourY = slope()*yourX + yIntercept();
+        return "(" + yourX + ", " + yourY + ")";
+    }
+    public String lineInfo(){
+        return "the slope ";
+    }
+
 }
 
 
--- src/LinearEquationsRunner.java
+++ src/LinearEquationsRunner.java
@@ -2,13 +2,30 @@ import java.util.Scanner;
 public class LinearEquationsRunner {
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
-       // LinearEquations eq1 = new LinearEquations(1,5,3,11);
-  //  System.out.println(eq1.distance() + "\n" + eq1.slope() + "\n" + eq1.equation() + "\n" + eq1.yIntercept());
+        // LinearEquations eq1 = new LinearEquations(1,5,3,11);
+        //  System.out.println(eq1.distance() + "\n" + eq1.slope() + "\n" + eq1.equation() + "\n" + eq1.yIntercept());
         System.out.println("Welcome!");
         System.out.println("Enter coordinate 1: ");// (1, 2)
-String XY1 = scan.nextLine();
-String X1 = XY1.substring(1,XY1.indexOf(","));
-//String Y1 = XY1.substring();
+        String XY1 = scan.nextLine();
+        int X1 =Integer.parseInt( XY1.substring(1,XY1.indexOf(",")));
+        int Y1 = Integer.parseInt(XY1.substring(XY1.indexOf(",")+ 1, XY1.indexOf(")")));
+        //System.out.print("This is X1: " + X1 + " and this is y1: " + Y1);
 
+        System.out.println("Enter coordinate 2: ");// (1, 2)
+        String XY2 = scan.nextLine();
+        int X2 = Integer.parseInt(XY2.substring(1,XY2.indexOf(",")));
+        int Y2 = Integer.parseInt(XY2.substring(XY2.indexOf(",")+ 1, XY2.indexOf(")")));
+     // System.out.print("This is X1: " + X2 + " and this is y1: " + Y2);
+
+LinearEquations eq1 = new LinearEquations(X1, Y1, X2, Y2);
+System.out.println("The two points are: " + XY1 + " and " + XY2 );
+System.out.println("The distance between the two points is: " + String.format("%.1f", eq1.distance()));
+System.out.println("The slope of this line is: " + String.format("%.2f", eq1.slope()));
+System.out.println("The equation of the line between these two points is: " + eq1.equation());
+System.out.println("The y-intercept of the line is: " + String.format("%.2f", eq1.yIntercept()));
+
+System.out.println("Enter a value for x: ");
+double newX = scan.nextDouble();
+System.out.println("The point on the line is: " + eq1.chooseVal(newX));
     }
 }
